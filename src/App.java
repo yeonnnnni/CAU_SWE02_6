30b72f98b88bbb60487b4bf71b95d3369d92eace
hs03291900 <peachee030@gmail.com>
2025-04-30 23:44:13 +0900

    first commit

---
+++ src/App.java
@@ -0,0 +1,8 @@
+import view.MainFrame;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        MainFrame frame = new MainFrame();
+        frame.setVisible(true);
+    }
+}
---
+++ src/view/MainFrame.java
@@ -0,0 +1,12 @@
+package view;
+
+import  javax.swing.*;
+
+public class MainFrame extends JFrame {
+    public MainFrame(){
+        setTitle("윷놀이게임");
+        setSize(1000, 1000);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+}
